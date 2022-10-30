df3920c953f280b06461545c3e0b2d77b4b7fdc0
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-30 19:08:47 +0800

    Add root nav

--- src/assets/styles/primereact.js
+++ src/assets/styles/primereact.js
@@ -1,3 +1,4 @@
-import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/themes/lara-light-teal/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
