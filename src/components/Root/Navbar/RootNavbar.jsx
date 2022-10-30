457fc96acd29a8b65ca2fcabd7be2e134c50072b
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-30 19:46:37 +0800

    Add onclicks

--- src/components/Root/Navbar/RootNavbar.jsx
+++ src/components/Root/Navbar/RootNavbar.jsx
@@ -1,15 +1,31 @@
 import { Button } from 'primereact/button'
 
 const RootNavbar = () => {
+  const handleLogo = () => {
+    window.location.assign('/')
+  }
+
+  const handleLogin = () => {
+    window.location.assign('/login')
+  }
+
+  const handleSignUp = () => {
+    window.location.assign('/signup')
+  }
+
   return (
     <div className="flex align-items-center justify-content-between m-4">
-      <div className="flex" style={{ cursor: 'pointer' }}>
+      <div className="flex" style={{ cursor: 'pointer' }} onClick={handleLogo}>
         <i className="pi pi-calendar-plus text-teal-500 text-3xl font-bold"></i>
         <p className="text-2xl text-teal-500 font-bold ml-3">Jivemed</p>
       </div>
       <div className="flex">
-        <Button className="p-button-text mr-3" label="Login" />
-        <Button label="Sign up" />
+        <Button
+          className="p-button-text mr-3"
+          label="Login"
+          onClick={handleLogin}
+        />
+        <Button label="Sign up" onClick={handleSignUp} />
       </div>
     </div>
   )
