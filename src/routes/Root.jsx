905c6290f10e5faacb0b42d1d9def1a6b1dfe121
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-30 17:57:14 +0800

    Restore files

---
+++ src/routes/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import wireframe from '../assets/images/wireframe.png'
+
+const Root = () => {
+  return (
+    <div>
+      Root
+      <img src={wireframe} alt="" />
+    </div>
+  )
+}
+
+export default Root
