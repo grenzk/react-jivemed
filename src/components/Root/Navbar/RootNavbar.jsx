91f18d65bf39b211ae0b62119d9db340e19daeaa
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-10-31 18:01:57 +0800

    Remove daisy ui

--- src/components/Root/Navbar/RootNavbar.jsx
+++ src/components/Root/Navbar/RootNavbar.jsx
@@ -11,23 +11,7 @@ const RootNavbar = () => {
     window.location.assign('/signup')
   }
 
-  return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl" onClick={handleLogo}>
-          Jivemed
-        </a>
-      </div>
-      <div className="navbar-end gap-2">
-        <button className="btn btn-ghost" onClick={handleLogin}>
-          Login
-        </button>
-        <button className="btn" onClick={handleSignUp}>
-          Get Started
-        </button>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default RootNavbar
