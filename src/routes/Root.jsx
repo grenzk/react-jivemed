8ff2bfa8fd5c06cce1398dc98b7cb12d7070e2cd
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-01 14:57:41 +0800

    Add root elements

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,9 +1,51 @@
+import {
+  Center,
+  Stack,
+  Group,
+  Title,
+  Text,
+  Button,
+  createStyles,
+} from '@mantine/core'
 import RootNavbar from '../components/Root/Navbar/RootNavbar.jsx'
+import { SIGN_UP_LINK } from '../services/constants/links.js'
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    maxWidth: 768,
+  },
+}))
 
 const Root = () => {
+  const { classes } = useStyles()
+
+  const handleGetStarted = () => window.location.assign(SIGN_UP_LINK)
+
   return (
     <>
       <RootNavbar />
+      <Center>
+        <Stack>
+          <Group p={30} className={classes.container}>
+            <Title>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </Title>
+          </Group>
+          <Group px={30} className={classes.container}>
+            <Text>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Consectetur repudiandae sit ipsa explicabo dolore saepe voluptate,
+              vel velit quo! Iure nobis voluptas in. Eligendi, voluptatum dolor
+              nisi fugit nihil quam?
+            </Text>
+          </Group>
+          <Group px={30} pt={30} className={classes.container}>
+            <Button size="xl" onClick={handleGetStarted}>
+              Get Started
+            </Button>
+          </Group>
+        </Stack>
+      </Center>
     </>
   )
 }
