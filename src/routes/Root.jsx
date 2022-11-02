188648ed7151a67023660c5f6196a4c368dc51b8
grenzk <74033991+grenzk@users.noreply.github.com>
2022-11-02 22:07:44 +0800

    Modify hero section

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -11,18 +11,10 @@ import {
 } from '@mantine/core'
 import RootNavbar from '../components/Root/Navbar/RootNavbar.jsx'
 import { SIGN_UP_LINK } from '../services/constants/links.js'
-// import { IconCheck } from '@tabler/icons';
+import { TbCheck } from 'react-icons/tb'
 import image from '../assets/img/hero.svg'
-import bgImage from '../assets/img/calendar.svg'
 
 const useStyles = createStyles((theme) => ({
-  root: {
-    backgroundSize: '40rem',
-    backgroundPosition: 'right',
-    backgroundRepeat: 'no-repeat',
-    backgroundImage: `url(${bgImage})`,
-  },
-
   inner: {
     display: 'flex',
     justifyContent: 'space-between',
@@ -43,8 +35,8 @@ const useStyles = createStyles((theme) => ({
   title: {
     color: theme.colorScheme === 'dark' ? theme.white : theme.black,
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontSize: 44,
-    lineHeight: 1.2,
+    fontSize: 50,
+    lineHeight: 1.1,
     fontWeight: 900,
 
     [theme.fn.smallerThan('xs')]: {
@@ -89,36 +81,32 @@ const Root = () => {
         <div className={classes.inner}>
           <div className={classes.content}>
             <Title className={classes.title}>
-              A <span className={classes.highlight}>modern</span> React <br />{' '}
-              components library
+              Skip the lines, and meet your doctor
             </Title>
-            <Text color="dimmed" mt="md">
-              Build fully functional accessible web applications faster than
-              ever – Mantine includes more than 120 customizable components and
-              hooks to cover you in any situation
+            <Text color="dimmed" mt="xl" size="lg">
+              No stress added. Focus on what really matters.
+              <br />
+              Getting your health checked.
             </Text>
 
             <List
               mt={30}
               spacing="sm"
-              size="sm"
+              size="md"
               icon={
                 <ThemeIcon size={20} radius="xl">
-                  {/* <IconCheck size={12} stroke={1.5} /> */}
+                  <TbCheck size={12} />
                 </ThemeIcon>
               }
             >
               <List.Item>
-                <b>TypeScript based</b> – build type safe applications, all
-                components and hooks export types
+                <b>Fast and easy</b>
               </List.Item>
               <List.Item>
-                <b>Free and open source</b> – all packages have MIT license, you
-                can use Mantine in any project
+                <b>Pay your appointments online</b>
               </List.Item>
               <List.Item>
-                <b>No annoying focus ring</b> – focus ring will appear only when
-                user navigates with keyboard
+                <b>Choose your preferred doctor based on your availability</b>
               </List.Item>
             </List>
 
