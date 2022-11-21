ac77eb3707375b42a6b87039b1e8787aabe3bd2e
grenzk <74033991+grenzk@users.noreply.github.com>
2022-11-21 19:10:12 +0800

    Create navbar and table component

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ import Root from './routes/Root'
 import Error from './routes/Error'
 import Auth from './routes/Auth'
 
+import Admin from './components/Admin/Admin'
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Root />} errorElement={<Error />} />
+      <Route path="/" element={<Admin />} errorElement={<Error />} />
       <Route path={SIGN_IN_LINK} element={<Auth />} caseSensitive />
       <Route path={SIGN_UP_LINK} element={<Auth />} caseSensitive />
     </>
