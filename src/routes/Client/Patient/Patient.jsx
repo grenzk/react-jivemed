198343c8897b3d7b8137eefaf5267f6e334bf5c4
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-23 19:31:32 +0800

    Add patient dashboard

--- src/routes/Client/Patient/Patient.jsx
+++ src/routes/Client/Patient/Patient.jsx
@@ -1,15 +1,18 @@
 import { useLocation } from 'react-router-dom'
 import {
   CLIENT_DASHBOARD_LINK,
+  CLIENT_AVAILABLE_SCHEDULES_LINK,
   CLIENT_DOCTORS_LINK,
   CLIENT_TRANSACTIONS_LINK,
-  CLIENT_AVAILABLE_SCHEDULES_LINK,
 } from '../../../services/constants/links'
+import ClientPatientDashboard from './ClientPatientDashboard'
 
 const Patient = () => {
   const location = useLocation()
 
   switch (location.pathname) {
+    case CLIENT_DASHBOARD_LINK:
+      return <ClientPatientDashboard />
   }
 }
 
