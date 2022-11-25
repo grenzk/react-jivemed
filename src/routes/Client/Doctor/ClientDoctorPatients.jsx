96a8a847e8db527b5f7e14ca6937491513ae3639
grenzk <74033991+grenzk@users.noreply.github.com>
2022-11-25 19:57:52 +0800

    Add patients table

--- src/routes/Client/Doctor/ClientDoctorPatients.jsx
+++ src/routes/Client/Doctor/ClientDoctorPatients.jsx
@@ -1,5 +1,11 @@
+import PatientsTable from '../../../components/Client/Doctor/Table/PatientsTable'
+
 const ClientDoctorPatients = () => {
-  return <div>ClientDoctorPatients</div>
+  return (
+    <div>
+      <PatientsTable />
+    </div>
+  )
 }
 
 export default ClientDoctorPatients
