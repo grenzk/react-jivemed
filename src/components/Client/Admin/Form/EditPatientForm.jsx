e73f361ca1bf63a9281380a9f8c824f6a558f708
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-25 20:02:48 +0800

    Add edit password

--- src/components/Client/Admin/Form/EditPatientForm.jsx
+++ src/components/Client/Admin/Form/EditPatientForm.jsx
@@ -1,16 +1,19 @@
 import { useForm } from '@mantine/form'
 import { Button, Group, TextInput, PasswordInput } from '@mantine/core'
-import { TbUser, TbMail } from 'react-icons/tb'
+import { TbUser, TbMail, TbLock } from 'react-icons/tb'
 
 const EditPatientForm = ({ patient, onSubmit }) => {
   const form = useForm({
     initialValues: {
       firstName: patient.first_name,
       lastName: patient.last_name,
+      password: '',
       email: patient.email,
     },
 
     validate: {
+      firstName: (value) => (value !== '' ? null : 'Invalid first name'),
+      lastName: (value) => (value !== '' ? null : 'Invalid last name'),
       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
     },
   })
@@ -18,16 +21,30 @@ const EditPatientForm = ({ patient, onSubmit }) => {
   return (
     <form
       onSubmit={form.onSubmit((values) => {
+        const checkPassword = () => {
+          if (values.password === '') {
+            return {
+              user: {
+                first_name: values.firstName,
+                last_name: values.lastName,
+                email: values.email,
+              },
+            }
+          } else {
+            return {
+              user: {
+                first_name: values.firstName,
+                last_name: values.lastName,
+                password: values.password,
+                email: values.email,
+              },
+            }
+          }
+        }
+
         onSubmit({
           id: patient.id,
-          values: {
-            user: {
-              first_name: values.firstName,
-              last_name: values.lastName,
-              email: values.email,
-              password: values.password,
-            },
-          },
+          values: checkPassword(),
         })
         form.reset()
       })}
@@ -37,6 +54,7 @@ const EditPatientForm = ({ patient, onSubmit }) => {
         <TextInput required label="Last Name" icon={<TbUser />} {...form.getInputProps('lastName')} />
       </Group>
       <TextInput required label="Email" mb="xl" icon={<TbMail />} {...form.getInputProps('email')} />
+      <PasswordInput label="Password" mb="xl" icon={<TbLock />} {...form.getInputProps('password')} />
       <Button variant="gradient" gradient={{ from: 'indigo', to: 'cyan' }} fullWidth={true} type="submit">
         Submit
       </Button>
