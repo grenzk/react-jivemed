6abf2088102e8d7ad60f891193a0c2d917c63c21
grenzk <74033991+grenzk@users.noreply.github.com>
2022-11-25 13:51:38 +0800

    Add transactions table

--- src/routes/Client/Patient/ClientPatientTransactions.jsx
+++ src/routes/Client/Patient/ClientPatientTransactions.jsx
@@ -1,5 +1,11 @@
+import TransactionsTable from '../../../components/Client/Patient/Table/TransactionsTable'
+
 const ClientPatientTransactions = () => {
-  return <div>ClientPatientTransactions</div>
+  return (
+    <div>
+      <TransactionsTable />
+    </div>
+  )
 }
 
 export default ClientPatientTransactions
