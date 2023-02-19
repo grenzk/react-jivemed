f7c6489848a73f358b817bc7ee58ef913c44e4a5
louisordonez <jonnlouisordonez@gmail.com>
2023-02-20 02:12:15 +0800

    Fix center loader style

--- src/components/CenterLoader.jsx
+++ src/components/CenterLoader.jsx
@@ -3,8 +3,8 @@ import { Container, Center, Loader } from '@mantine/core'
 const CenterLoader = () => {
   return (
     <Container sx={{ minWidth: 1000, height: 450 }}>
-      <Center style={{ margin: '10rem' }}>
-        <Loader size="xl" />
+      <Center>
+        <Loader size="xl" style={{ marginTop: '10rem' }} />
       </Center>
     </Container>
   )
