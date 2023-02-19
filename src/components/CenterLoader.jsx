d52bbc4fe8ced882a2f187026849fc1f81796cce
louisordonez <72607877+louisordonez@users.noreply.github.com>
2023-02-20 02:04:36 +0800

    Update CenterLoader.jsx

--- src/components/CenterLoader.jsx
+++ src/components/CenterLoader.jsx
@@ -2,7 +2,7 @@ import { Container, Center, Loader } from '@mantine/core'
 
 const CenterLoader = () => {
   return (
-    <Container sx={{ minWidth: 1000 }} sx={{ height: 450 }}>
+    <Container sx={{ minWidth: 1000, height: 450 }}>
       <Center style={{ margin: '10rem' }}>
         <Loader size="xl" />
       </Center>
